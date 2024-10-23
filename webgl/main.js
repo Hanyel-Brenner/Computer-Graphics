783528bed1310f4c10db695cefb7fbd3d398a73e
Hanyel-Brenner <125314727+Hanyel-Brenner@users.noreply.github.com>
2024-10-22 22:43:21 -0300

    Update main.js

--- webgl/main.js
+++ webgl/main.js
@@ -1,8 +1,9 @@
-function main(){
+function main() {
+
     const canvas = document.querySelector('canvas');
     const gl = canvas.getContext('webgl');
 
-    if(!gl){ 
+    if (!gl) {
         throw new Error('WebGL not supported');
     }
 
@@ -15,20 +16,21 @@ function main(){
     `
 
     const fragmentShaderSrc = `
-    attribute vec3 vColor;
     precision mediump float;
 
     void main(){
         gl_FragColor = vec4(1.0,0.0,1.0,1.0);
     }
     `
+
+
     //create shaders and attach to program 
     const program = gl.createProgram();
 
     const vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderSrc)
     gl.compileShader(vertexShader);
-    if(!gl.getShaderParameter(vertexShader,gl.COMPILE_STATUS)){
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
         throw new Error(gl.getShaderInfoLog(vertexShader));
     }
     gl.attachShader(program, vertexShader);
@@ -36,7 +38,7 @@ function main(){
     const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderSrc)
     gl.compileShader(fragmentShader);
-    if(!gl.getShaderParameter(fragmentShader,gl.COMPILE_STATUS)){
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
         throw new Error(gl.getShaderInfoLog(fragmentShader));
     }
     gl.attachShader(program, fragmentShader);
@@ -44,10 +46,37 @@ function main(){
     //finally link attached shaders, program should be ready to use for drawing
     gl.linkProgram(program);
 
-    if(!gl.getProgramParameter(program)){
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
         throw new Error(gl.getProgramInfoLog(program));
     }
 
-}
+    gl.useProgram(program);
+    // vertex positions for a square
+    const squareVertexPositions = new Float32Array([
+        -0.5, 0.5,
+        0.5, -0.5,
+        0.5, 0.5,
+        -0.5, 0.5,
+        0.5, -0.5,
+        -0.5, -0.5,
+    ]);
+
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(positionBuffer, new Float32Array(squareVertexPositions), gl.STATIC_DRAW);
+
+    /*location refers to the location of the attributes defined in shader or fragment glsl
+    *In this case refers to the a_position attribute defined in shader program
+    *we can get this reference and work with it the following way*/
+    const positionLocation = gl.getAttribLocation(program,'a_position');
+    gl.enableVertexAttribArray(positionLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+}   
 
 main();
