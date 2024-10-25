dbb8aa68aa414c71b27ddad32f1e0b006d7fd433
Hanyel-Brenner <125314727+Hanyel-Brenner@users.noreply.github.com>
2024-10-25 11:54:23 -0300

    added color buffer binding and refactored circle and rectangle functions
    now the webgl context is passed as parameter to the functions and bufferData is set inside the function

--- webgl/main.js
+++ webgl/main.js
@@ -10,61 +10,53 @@ function main() {
     const vertexShaderSrc = document.getElementById('vertexShader').text;
     const fragmentShaderSrc = document.getElementById('fragmentShader').text;
 
-    //create shaders and attach to program 
     const vertexShader = generateShader(gl,gl.VERTEX_SHADER,vertexShaderSrc);
     const fragmentShader = generateShader(gl,gl.FRAGMENT_SHADER,fragmentShaderSrc);
     const program = generateProgram(gl, vertexShader, fragmentShader);
     
     gl.useProgram(program);
     
-    const vertexPositions = new Float32Array([
-        -0.5, 0.5,
-        0.5, -0.5,
-        0.5, 0.5,
-        -0.5, 0.5,
-        0.5, -0.5,
-        -0.5, -0.5
-    ]);
-    
-    const vertexColors = new Float32Array([
-        1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.0, 1.0,
-        1.0, 1.0, 1.0,
-        1.0, 0.0, 0.0,
-        1.0, 0.0, 0.0
-    ]);
-
-    var numberOfCirclePoints = 50000;
-    
-    const generatedVertexPositions = new Float32Array(rectangle(0.0,0.0, 0.5, -0.8))//circle(-0.3,-0.3,0.5,numberOfCirclePoints); rectangle(0.0,0.0, 0.5, -0.8)
-    const generatedColor = new Float32Array();
-
     const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER,generatedVertexPositions, gl.STATIC_DRAW);
+    const colorBuffer = gl.createBuffer();
 
-   /* const colorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertexColors, gl.STATIC_DRAW)*/
-
-    /*location refers to the location of the attributes defined in shader or fragment glsl
-    *In this case refers to the a_position attribute defined in shader program
-    *we can get this reference and work with it the following way*/
+/*
+*location refers to the location of the attributes defined in shader or fragment glsl
+*/
     const positionLocation = gl.getAttribLocation(program,'a_position');
     gl.enableVertexAttribArray(positionLocation);
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
 
-    /*const colorLocation = gl.getAttribLocation(program,'color');
+    const colorLocation = gl.getAttribLocation(program,'color');
     gl.enableVertexAttribArray(colorLocation);
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false,0,0);*/
-    
+    gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
+
+    /*
+    *clear screen
+    */
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    gl.drawArrays(gl.TRIANGLES, 0, numberOfCirclePoints*6);
+
+    /*
+    *bind buffer and set buffer data each at a time(position and color)
+    */
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    setRectangleVertices(gl,0,0,0.5,0.8);
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    setRectangleColor(gl,1.0,0.0,0.0);
+
+    gl.drawArrays(gl.TRIANGLES,0,6);
+
+
+    var nOfPoints = 3000;
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    setCircleVertices(gl, -0.3, -0.3, 0.5, nOfPoints);
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    setCircleColor(gl, 0.0, 0.0, 1.0, nOfPoints);    
+
+    gl.drawArrays(gl.TRIANGLES, 0, nOfPoints*6);
 }   
 
 main();
--- webgl/shapes.js
+++ webgl/shapes.js
@@ -5,9 +5,9 @@
 *data is put in dynamic array so must be converted to a typed float array outside of function
 *not supposed to be used directly, use with the function
 */
-function circle(c_x, c_y, radius, numberOfPoints){
+function setCircleVertices(gl,c_x, c_y, radius, numberOfPoints){
   var angle = (2 * Math.PI)/numberOfPoints; 
-  var vertexes = [];
+  var vertices = [];
   
   for(i=0; i<numberOfPoints; i++){
     var x = radius * Math.cos(angle*i) + c_x;
@@ -15,43 +15,66 @@ function circle(c_x, c_y, radius, numberOfPoints){
     var x2 = radius * Math.cos(angle*(i+1)) + c_x;
     var y2 = radius * Math.sin(angle*(i+1)) + c_y;
     
-    vertexes.push(x);
-    vertexes.push(y);
-    vertexes.push(x2);
-    vertexes.push(y2);
-    vertexes.push(0.0) + c_x;
-    vertexes.push(0.0) + c_y; 
+    vertices.push(x);
+    vertices.push(y);
+    vertices.push(x2);
+    vertices.push(y2);
+    vertices.push(0.0) + c_x;
+    vertices.push(0.0) + c_y; 
   }
-  return vertexes;
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 }
 
 /*
-*generates vertex array for rectangle
+*generates vertex array for rectangle and binds the data to the array buffer
 *not optmized, since it has duplicated vertex data
 *data is put in dynamic array so must be converted to a typed float array outside of function
 */
-function rectangle(x,y,width, height){
-  vertexes = [];
-  vertexes.push(x);
-  vertexes.push(y);
-  vertexes.push(x + width);
-  vertexes.push(y);
-  vertexes.push(x);
-  vertexes.push(y + height);
+function setRectangleVertices(gl,x,y,width, height){
+  var vertices = [];
+  vertices.push(x);
+  vertices.push(y);
+  vertices.push(x + width);
+  vertices.push(y);
+  vertices.push(x);
+  vertices.push(y + height);
 
-  vertexes.push(x + width);
-  vertexes.push(y);
-  vertexes.push(x);
-  vertexes.push(y + height);
-  vertexes.push(x + width);
-  vertexes.push(y + height);
+  vertices.push(x + width);
+  vertices.push(y);
+  vertices.push(x);
+  vertices.push(y + height);
+  vertices.push(x + width);
+  vertices.push(y + height);
 
-  return vertexes;
+  console.log(vertices);
+  console.log(new Float32Array(vertices));
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
 }
 
 /* 
 *generates the color array for a circle with given number of points
 */
-function circleColor(numberOfPoints){
+function setCircleColor(gl, c1,c2,c3 , numberOfPoints){
+  var colorArray = [];  
+  for(i=0; i<numberOfPoints*6; i++){
+    colorArray.push(c1);
+    colorArray.push(c2);
+    colorArray.push(c3); 
+  }
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorArray), gl.STATIC_DRAW);
+}
+
+function setRectangleColor(gl,c1,c2,c3){
+  var colorArray = [];
+
+  for(i=0; i<6; i++){
+    colorArray.push(c1);
+    colorArray.push(c2);
+    colorArray.push(c3);  
+  }
+  console.log(colorArray);
+  console.log(new Float32Array(colorArray));
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorArray), gl.STATIC_DRAW);
 
 }
