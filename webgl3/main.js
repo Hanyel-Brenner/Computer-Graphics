69647f1a9deeee813c76679a643092af7c522534
Hanyl <hanyelb46@gmail.com>
2024-11-11 01:31:55 -0300

    added glMatrix library to project in gl-matrix-min.js file

--- webgl3/main.js
+++ webgl3/main.js
@@ -36,88 +36,16 @@ function main() {
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
 
+    const transfMatrixLoc = gl.getUniformLocation(program, 'matrix');
+    const matrix = mat4.create();
+    console.log(matrix);
     /*
     *clear screen
     */
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.viewport(0, 0, canvas.width, canvas.height);
-
-    /*
-    *draw car in middle of screen
-    */
-/*
-    for(let i=0; i<carWheels.length; i++)
-    {
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-        setCircleVertices(gl,carWheels[i][0], carWheels[i][1], carWheels[i][2], N_OF_CIRCLE_POINTS);
-        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-        setCircleColor(gl,carWheelsColor[i][0],carWheelsColor[i][1],carWheelsColor[i][2], N_OF_CIRCLE_POINTS);
-
-        gl.drawArrays(gl.TRIANGLES,0, N_OF_CIRCLE_POINTS*6);
-    }
   
-    for(let i=0; i<carTriangles.length; i++)
-    {
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-        setRectangleVertices(gl,carTriangles[i][0],carTriangles[i][1],carTriangles[i][2],carTriangles[i][3]);
-        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-        setRectangleColor(gl,carTrianglesColor[i][0],carTrianglesColor[i][1],carTrianglesColor[i][2]);
-        
-        gl.drawArrays(gl.TRIANGLES,0,6);
-    }
-*/
-/*
-    draw flower in the middle of the screen
-*/
-
-   /* for(let i=0; i<flowerCircles.length; i++)
-    {
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-        setCircleVertices(gl,flowerCircles[i][0], flowerCircles[i][1], flowerCircles[i][2], N_OF_CIRCLE_POINTS)
-        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-        setCircleColor(gl,flowerCircleColor[i][0],flowerCircleColor[i][1],flowerCircleColor[i][2], N_OF_CIRCLE_POINTS);
-
-        gl.drawArrays(gl.TRIANGLES,0, N_OF_CIRCLE_POINTS*6);
-    }
-
-    for(let i=0; i<flowerTriangles.length; i++)
-    {
-            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-            setRectangleVertices(gl,flowerTriangles[i][0],flowerTriangles[i][1],flowerTriangles[i][2],flowerTriangles[i][3]);
-            gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-            setRectangleColor(gl,flowerTrianglesColor[i][0],flowerTrianglesColor[i][1],flowerTrianglesColor[i][2]);
-
-            gl.drawArrays(gl.TRIANGLES,0,6);
-    } */
-
-    for(let i=0; i<clownCircles.length; i++)
-        {
-            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-            setCircleVertices(gl,clownCircles[i][0], clownCircles[i][1], clownCircles[i][2], N_OF_CIRCLE_POINTS)
-            gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-            setCircleColor(gl,clownCirclesColor[i][0],clownCirclesColor[i][1],clownCirclesColor[i][2], N_OF_CIRCLE_POINTS);
-    
-            gl.drawArrays(gl.TRIANGLES,0, N_OF_CIRCLE_POINTS*6);
-        }
-
-    for(let i=0; i<clownTriangles.length; i++)
-        {
-            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-            setRectangleVertices(gl,clownTriangles[i][0],clownTriangles[i][1],clownTriangles[i][2],clownTriangles[i][3]);
-            gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-            setRectangleColor(gl,clownTrianglesColor[i][0],clownTrianglesColor[i][1],clownTrianglesColor[i][2]);
-        
-            gl.drawArrays(gl.TRIANGLES,0,6);
-        }
-    
-    /*var nOfPoints = 3000;
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    setCircleVertices(gl, -0.3, -0.3, 0.5, nOfPoints);
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    setCircleColor(gl, 0.0, 0.0, 1.0, nOfPoints);   
-    gl.drawArrays(gl.TRIANGLES, 0, nOfPoints*6);
-    */
 }   
 
 main();
