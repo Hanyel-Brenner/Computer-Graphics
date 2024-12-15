c81171106e6cb0f9b7eb9ae3ae0e9e5e040c99c5
Hanyl <hanyelb46@gmail.com>
2024-12-15 19:44:31 -0300

    now i can render cube in 3d, but it seems to be upside-down

---
+++ webgl6Viewing3d/main.js
@@ -0,0 +1,125 @@
+const N_OF_CIRCLE_POINTS = 100;
+const MAX_POINTS = 3;
+/*
+*keysPressed maintains a hashmap of the state of keys pressed at a given moment with a boolean mapped to the keyCode of a keydown event
+*/
+keysPressed = {}; 
+
+const colors = [[1.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0],
+                [0.0 , 0.0, 1.0],
+                [1.0, 1.0, 0.0],
+                [1.0, 0.0, 1.0],
+                [0.0, 1.0, 1.0]];
+
+const cubePosition = setCubeVertices();
+const cubeColor = setCubeFaceColors(colors);
+
+function main() {
+
+    const body = document.querySelector('body');
+    const canvas = document.getElementById('canvas');
+    const gl = canvas.getContext('webgl', { preserveDrawingBuffer: true } );
+
+    if (!gl) {
+        throw new Error('WebGL not supported');
+    }
+
+    const vertexShaderSrc = document.getElementById('vertexShader').text;
+    const fragmentShaderSrc = document.getElementById('fragmentShader').text;
+
+    const vertexShader = generateShader(gl,gl.VERTEX_SHADER,vertexShaderSrc);
+    const fragmentShader = generateShader(gl,gl.FRAGMENT_SHADER,fragmentShaderSrc);
+    const program = generateProgram(gl, vertexShader, fragmentShader);
+    
+    gl.useProgram(program);
+
+    gl.enable(gl.DEPTH_TEST);
+    
+    const positionBuffer = gl.createBuffer();
+    const colorBuffer = gl.createBuffer();
+
+/*
+*location refers to the location of the attributes defined in shader or fragment glsl
+*/
+    const positionLocation = gl.getAttribLocation(program,'position');
+    gl.enableVertexAttribArray(positionLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+    const colorLocation = gl.getAttribLocation(program,'color');
+    gl.enableVertexAttribArray(colorLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
+
+
+/*
+*Event listeners for keyboard or mouse
+*/    
+    body.addEventListener("keydown", function(event){
+        keyboardPressDown(event);
+    },false);
+
+    body.addEventListener("keyup", function(event){
+        keyboardPressUp(event);
+    },false);
+
+/*
+*Definition of transformation matrix, initiating it with identity matrix
+*/    
+    const transfMatrixLoc = gl.getUniformLocation(program, 'matrix');
+    gl.uniformMatrix4fv(transfMatrixLoc, false, mat4.create());
+
+/*
+*clear screen
+*/
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+
+    var matrix = mat4.create();
+    var vcMatrix = mat4.create();
+    var wvMatrix = mat4.create();
+    var p0 = [1.0, 1.0, 1.0];
+    var pRef = [0.0 , 0.0, 0.0];
+    var V = [0.0, 1.0, 0.0];
+
+    var xw_min = -2.0;
+    var xw_max = 2.0;
+    var yw_min = -2.0;
+    var yw_max = 2.0;
+    var z_far = -4.0; 
+    var z_near = 0.0;
+
+    function render(){
+
+        vcMatrix = get3DViewingMatrix(p0, pRef, V);
+        wvMatrix = getOrtographicMatrix(xw_min, xw_max, yw_min, yw_max, z_far, z_near);
+        mat4.multiply(matrix, wvMatrix, vcMatrix);
+
+        gl.uniformMatrix4fv(transfMatrixLoc, false, matrix);
+
+        gl.clearColor(1.0, 1.0, 1.0, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubePosition), gl.STATIC_DRAW);
+        //gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.5, 0.5, 0.0]), gl.STATIC_DRAW);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubeColor), gl.STATIC_DRAW);
+        //gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0]), gl.STATIC_DRAW);
+
+
+        gl.drawArrays(gl.TRIANGLES, 0, cubePosition.length/3);
+        
+        requestAnimationFrame(render);
+    }
+
+    render();
+    
+}
+
+main();
+
+
